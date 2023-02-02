77a5de090fefc308283e84e7282f03f7978cef5c
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2023-02-02 21:50:31 +0300

    Create Virtual Enviroment (Sanal Ortam) ve (Package Managment) Paket Yönetimi.py

---
+++ Virtual Enviroment (Sanal Ortam) ve (Package Managment) Paket Yönetimi.py
@@ -0,0 +1,34 @@
+# Sanal ortamların listelenmesi:
+# conda env list
+
+# Sanal ortamların oluşturulması:
+# conda create -n myenv
+
+# Sanal ortamı aktif etme:
+# conda activate myenv
+
+# Yüklü paketlerin listelenmesi:
+# conda list
+
+# Paket yükleme:
+# conda install numpy
+
+# aynı anda birden fazla paket yükleme:
+# conda install numpy scipy pandas
+
+# Paket silme:
+# conda remove package_name
+
+# Belirli bir versiyona göre paket yükleme:
+# conda install numpy=1.20.1
+
+
+# Paket yükseltme:
+# conda upgrade numpy
+
+# Tüm paketleri yükseltme:
+# conda upgrade -all
+
+
+# Bütün kullanılan versiyonları, paketleri, kütüphanleri dışa aktarmak için:
+# conda env export > environment.yaml
