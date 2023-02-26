9b5be92a0882ff1c99e8f028f5d44f8e3086b642
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2023-02-26 14:45:10 +0300

    Create 1.Hafta_Notları.py

---
+++ 1.Hafta_Notları.py
@@ -0,0 +1,24 @@
+"""
+Veri Yapıları(Nesneler):
+    ! Fonkyonel olarak listeleri kullanacağız en çok.
+    Geçici bellekte (RAM) farklı türlerde değerler tutmak için veri yapılarını kullanıyoruz.
+    Bir nevi somutlaştırma.
+    Programlamanın 4 ana temel yapıları var:
+    1: Nesneler (Veri Yapıları) En öenmlisi: Boolean, En sık kullancak olan: list
+    Numpy array i bir veri yapısıdır!
+    Örnek olarak. List yerine pandas dataframini kullamak hem hız hemde hemde veri saklama şekli ve
+     vektörel işlemler olarak çok daha avantajlı
+    2: Fonksiyonlar
+    Belirli bir görevi yerine getiren kod parçacığı.
+    :parameter: fonksiyon tanımlanırken kullanılır
+    :argument: fonksiyon çağırıken kullanılır
+    return kullanılmalı
+    default: Ön tanımlı argüman. Belirli tanımlı fonksiyonların kullanımı hızlandırıcı bir görev görür
+    docstring: fonksiyon kullanım kılavuzu. Bir fonksiyonon dökümantasyonu.
+    Fonksiyon method farkı: Görevleri itibari ile aynıdır.
+    Methodlar, veri yapıları ile ilintilidir. Sınıflar içindedir. Onlarla kullanılır.
+    Fonksşyonlar bağımsızdır.
+    3: Karar kontorol yapılaru
+    4: Döngüler
+
+"""
