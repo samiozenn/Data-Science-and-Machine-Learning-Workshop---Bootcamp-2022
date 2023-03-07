e5bbf837459fce79ca6182e5fe16ba76ac83543c
Sami OZEN <74319336+samiozenn@users.noreply.github.com>
2023-03-07 19:35:47 +0300

    Create 2. Hafta Notları.py

---
+++ 2. Hafta Notları.py
@@ -0,0 +1,58 @@
+"""
+np tek tipte(fix type) veri saklıyoruz. Bu yüzden hızlı. floatken int almaz.
+vektörel demek lineer cebir işlemleri yapabilmek.
+döngü yazmak yerine np a yaptırıyoruz.
+ 
+np kütüphansine katkıda bulunmak istiyorsak git hub a yüklemek gerekir.
+(Herhangi bir kütüphanye bir fonksiyon eklemek gibi)
+
+Gelişmiş Fonksiyonel Keşifçi veri analizi:
+Değişkenleri daha kolay analiz etmmeize yarar.
+
+Numpy vs List:
+verimli veri saklama
+vektrörel işlemlr (yüksek seviye)
+Çok daha hızlı
+döngü yazmadan array işlemleri yapabiliriz.
+
+Fancy İndex:
+Pandas uygulamaları senior gibi algılamak istiyorsak fancy index kilit noktlardan birisi.
+döngü yazmaya gerek kalmadan elimizde var olan true false arrayini,
+kullanarak dataframe içinden toplu olarak seçim yapmayı sağlar.
+Herhangi bir şekilde oluşabilecek true false arrayini dataframe içinden çekip almaya yarar.
+True false arayi döner.
+
+Pandas vs Numpy:
+pandas farklı veri yapılarını(tiplerini) tutuyor.
+pandasta indexlere mudahele edilebiliyor, değiştirebiliyoruz.
+
+
+machine learning ksımına geçince bunları daha detaylı görülecek
+
+loc & iloc:
+loc: label base
+Satır ve sütunların labelların mutlak isimlarini yada sıralrını kullanarak.
+yada sıralamalrını olduğu şekilde kullarak yapılan seçim.
+
+iloc: sıra numalarına göre
+index base kullanarak yapılan seçim
+
+toplulaştırma ve gruplaştırma(aggregation & Grouping):
+
+veri görselleştirme: 
+seaborn vs matplotlib:
+seaborn üst seviye, matplotlib alt seviye
+
+kategorik değişkenleri görselleştirme için kullanılabilecek teknikler?
+-stün grafiği: 
+- barplot graafiği
+- countplot grafiği
+
+sayısal değişken görselleştirme için kullanılacabildcek teknikler?
+
+-histogram: Değer aralıklarında yer alan değer sayısı. aralıkların frekanslarının gösterimidir.
+
+- boxplot
+
+
+"""
